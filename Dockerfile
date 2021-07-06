3ba59b973bec9e09b03b31887518ce9e57092400
Benedikt Bergenthal <benedikt@kdrennert.de>
2021-07-06 10:27:36 +0200

    updated Dockerfile for automated build
    Signed-off-by: Benedikt Bergenthal <benedikt@kdrennert.de>

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ WORKDIR /app
 COPY package*.json .
 RUN yarn install
 COPY app.js .
-COPY customers.js .
-COPY .env .
 CMD ["node", "app.js"]
--- customers.js
+++ customers.js
@@ -1,7 +1,7 @@
 const customers = [
     {
         name: "Imflow Doe",
-        apiKey: "HJ6kk2rZ2fYmvw9wju",
+        apiKey: "HJ6kk2rZ2fYmvw9wj",
         recipient: "benedikt.bergenthal@imflow.me",
     },
 ]
