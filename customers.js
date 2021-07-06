bd401daaa0bcbaab22d3e64c396fad34bc690aa0
Michel Wächter <82541652+michelwaechterimflow@users.noreply.github.com>
2021-07-06 13:32:32 +0200

    changing example mails.

--- customers.js
+++ customers.js
@@ -2,7 +2,7 @@ const customers = [
     {
         name: "Imflow Doe",
         apiKey: "HJ6kk2rZ2fYmvw9wju",
-        recipient: [ "benedikt.bergenthal@imflow.me", "michel.waechter@imflow.me" ],
+        recipients: [ "jon@doe.com", "jane@doe.com" ],
     },
 ]
 
